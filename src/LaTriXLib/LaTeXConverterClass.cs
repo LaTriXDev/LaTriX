9430476ca2666409057e053e49a357de7f06db43
Supergecki <46622920+Supergecki@users.noreply.github.com>
2024-07-14 14:49:47 +0200

    Alignment-Enumeration
    Es hat sich als sinnvoll erwiesen, mit einer Alignment-Enumeration zu arbeiten, um die Format-Spezifikationen der Matrix (links-/rechtsbündig oder zentriert) kommunizieren zu können. Passend dazu erscheint eine Konversionsmethode zwischen dem Alignment und dem zugehörigen Formatcharacter. Es handelt sich hierbei um eine kleine Änderung im Klassendesign, daher hat dies Einfluss auf #2 .

--- src/LaTriXLib/LaTeXConverterClass.cs
+++ src/LaTriXLib/LaTeXConverterClass.cs
@@ -5,5 +5,23 @@ namespace LaTriXLib;
 
 public static class LaTeXConverter
 {
+    public enum Alignment
+    {
+        left, center, right
+    }
 
+    private static char GetAlignmentChar(Alignment alignment) // Konvertiert ein Alignment in den LaTeX-Specifier (l, c oder r)
+    {
+        switch (alignment)
+        {
+            case Alignment.left:
+                return 'l';
+            case Alignment.right:
+                return 'r';
+            case Alignment.center:
+                return 'c';
+            default:
+                return 'c';
+        }
+    }
 }
